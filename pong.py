6de933d9592ae2b22093366f8f288006f72e61e3
Aditya S <asrinivas2002@gmail.com>
2023-07-27 14:02:27 +0530

    Added Middle Lines

--- pong.py
+++ pong.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 
-#game specifications
+#Game specifications
 
 window_width,window_height = 900,600
 
@@ -12,16 +12,19 @@ pygame.display.set_caption('PONG')
 
 font = pygame.font.SysFont("Comic Sans MS", 25, True)
 
-paddle_color = (255,255,255)
+paddle_color = (255, 255, 255)
+ball_color = (50,255,255)
 white = paddle_color
-black = (0,0,0)
+black = (0, 0, 0)
 
 
 clock = pygame.time.Clock()
-frame_rate = 60
+frame_rate = 30
+
 
 #Ball Class
 class Ball:
+
     def __init__(self, posx, posy, radius, speed, color):
         self.posx = posx
         self.posy = posy
@@ -40,17 +43,15 @@ class Ball:
         self.posx += self.speed * self.xFac
         self.posy += self.speed * self.yFac
 
-        if self.posx  <=0 or self.resetVar:
-            self.resetVar = 0 
-            return 1
-
         if self.posy <= 0 or self.posy >= window_height:
-            self.yFac *=-1
-        
+            self.yFac *= -1
+
+        if self.posx <= 0 and self.resetVar:
+            self.resetVar = 0
+            return 1
         elif self.posx >= window_width and self.resetVar:
             self.resetVar = 0
             return -1
-        
         else:
             return 0
 
@@ -59,12 +60,13 @@ class Ball:
         self.posy = window_height // 2
         self.xFac *= -1
         self.resetVar = 1
-    
+
     def hit(self):
         self.xFac *= -1
-    
+
     def getRect(self):
         return self.ball
+
 
 #Paddle Class
 class Paddle:
@@ -76,11 +78,11 @@ class Paddle:
         self.height = height
         self.speed = speed
         self.color = color
-        self.rect = pygame.Rect(posx, posy, width, height)
-        self.blit = pygame.draw.rect(window,self.color,self.rect)
+        self.prect = pygame.Rect(posx, posy, width, height)
+        self.plit = pygame.draw.rect(window,self.color,self.prect)
 
     def display(self):
-        self.blit = pygame.draw.rect(window, self.color, self.rect)
+        self.plit = pygame.draw.rect(window, self.color, self.prect)
 
     def update(self,Yfac):
         self.posy = self.posy + self.speed * Yfac
@@ -91,7 +93,7 @@ class Paddle:
         elif self.posy + self.height >= window_height:
             self.posy = window_height - self.height
 
-        self.rect = (self.posx, self.posy, self.width, self.height)
+        self.prect = (self.posx, self.posy, self.width, self.height)
 
     def displayScore(self, text, score, x, y, color):
         text = font.render(text + str(score), True, color)
@@ -101,16 +103,34 @@ class Paddle:
         window.blit(text, textRect)
 
     def getRect(self):
-        return self.rect
+        return self.prect
+
+# Middle Lines
+def draw_lines():
+    line_thickness = 3
+    line_length = 30
+    gap_length = 20
+
+    num_lines = window_height // (line_length + gap_length)
+
+    start_y = (window_height - (num_lines * (line_length + gap_length))) // 2
+
+    for i in range(num_lines):
+        y = start_y + i * (line_length + gap_length)
+
+        pygame.draw.line(window, white, (window_width // 2, y),(window_width // 2, y + line_length // 2),line_thickness)
+
+        pygame.draw.line(window, white,(window_width // 2, y + line_length // 2 + gap_length),(window_width // 2, y + line_length), line_thickness)
+
 
 
 #Game
 def main():
     run = True
 
-    paddle1 = Paddle(20,0,10,100,10,paddle_color)
-    paddle2 = Paddle(window -30, 0, 10, 100, 10, paddle_color)
-    ball = Ball(window_width // 2 , window_height // 2, 7, 7,white)
+    paddle1 = Paddle(20, 0, 10, 100, 10, paddle_color)
+    paddle2 = Paddle(window_width -30, 0, 10, 100, 10, paddle_color)
+    ball = Ball(window_width // 2 , window_height // 2, 7, 7,ball_color)
 
     paddleList = [paddle1, paddle2]
     paddle1Score,paddle2Score = 0, 0
@@ -155,12 +175,13 @@ def main():
         if point:
             ball.reset()
 
-            paddle1.display()
-            paddle2.display()
-            ball.display()
+        draw_lines()
+        paddle1.display()
+        paddle2.display()
+        ball.display()
 
-        paddle1.displayScore("Paddle 1 : ", paddle1Score, 100, 20, white)
-        paddle2.displayScore("Paddle 2 : ", paddle2Score, window_width - 100, 20, white)
+        paddle1.displayScore("Player 1 : ", paddle1Score, 100, 20, white)
+        paddle2.displayScore("Player 2 : ", paddle2Score, window_width - 100, 20, white)
 
         pygame.display.update()
         clock.tick(frame_rate)
